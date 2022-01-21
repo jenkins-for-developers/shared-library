003a368e68972a857c1021b75a6e99174be7e167
pelvik <pelvik@centrum.cz>
2022-01-21 14:34:40 +0100

    Update mvnExec.groovy

--- vars/mvnExec.groovy
+++ vars/mvnExec.groovy
@@ -1,6 +1,10 @@
 void call(Map config = [:]) {
     echo "Provede mvn build. Mapa by měla obsahovat mvn verzi, jdk verzi a mvn goals"
-        withMaven(jdk: config.get("jdk.version"), maven: config.get("mvn.version")) {
-          sh "mvn $config.get("mvn.goals")"
+    
+        echo "config.get("jdkVersion")"
+        echo "config.get("mvnVersion")"
+        echo "config.get("mvnGoals")"
+        withMaven(jdk: config.get("jdkVersion"), maven: config.get("mvnVersion")) {
+          sh "mvn $config.get("mvnGoals")"
         }
 }
